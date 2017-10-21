929521514510135f38258458eb8c23cf5a4538d1
Rheinard Korf <rheinard@thekorfs.com>
2017-10-22 03:42:53 +1100

    Add forever loop with timeout

--- cmd/sync-wp-repo/main.go
+++ cmd/sync-wp-repo/main.go
@@ -3,20 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/xwp/go-tide/wp"
+	"time"
 )
 
 func main() {
 
-	// Example Usage
-	// @todo Remove this example
-	fmt.Println("Getting plugin data from WordPress.org repo.")
-	fmt.Println("============================================")
-	repoPlugins, err := repo.RequestPlugins("updated", 5, 1)
-	if err == nil {
-		fmt.Printf("Current Page: %d \nTotal Pages: %d \nTotal Plugins: %d\n",
-			repoPlugins.Info.Page,
-			repoPlugins.Info.Pages,
-			repoPlugins.Info.Results)
+	// Forever loop with time.Sleep() to give the fetching a break.
+	for {
+
+		// Example Usage
+		// @todo Remove this example
+		fmt.Println("Getting plugin data from WordPress.org repo.")
+		fmt.Println("============================================")
+		repoPlugins, err := repo.RequestPlugins("updated", 5, 1)
+		if err == nil {
+			fmt.Printf("Current Page: %d \nTotal Pages: %d \nTotal Plugins: %d\n",
+				repoPlugins.Info.Page,
+				repoPlugins.Info.Pages,
+				repoPlugins.Info.Results)
+		}
+		fmt.Println("Done.")
+
+		// Wait before we go again.
+		time.Sleep(time.Minute)
 	}
-	fmt.Println("Done.")
 }
