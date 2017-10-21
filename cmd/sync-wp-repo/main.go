b1612defdd4691e644a03a9332e5e4b77cc003b8
Rheinard Korf <rheinard@thekorfs.com>
2017-10-22 03:25:22 +1100

    Improve example.

--- cmd/sync-wp-repo/main.go
+++ cmd/sync-wp-repo/main.go
@@ -9,11 +9,14 @@ func main() {
 
 	// Example Usage
 	// @todo Remove this example
+	fmt.Println("Getting plugin data from WordPress.org repo.")
+	fmt.Println("============================================")
 	repoPlugins, err := repo.RequestPlugins("updated", 5, 1)
 	if err == nil {
-		fmt.Printf("Current Page: %d \nTotal Pages: %d \nTotal Plugins: %d",
+		fmt.Printf("Current Page: %d \nTotal Pages: %d \nTotal Plugins: %d\n",
 			repoPlugins.Info.Page,
 			repoPlugins.Info.Pages,
 			repoPlugins.Info.Results)
 	}
+	fmt.Println("Done.")
 }
