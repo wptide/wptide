33eae83e64ebc3d3d6bff9d583e136b42da60315
Rheinard Korf <rheinard@thekorfs.com>
2018-01-22 11:31:29 +1100

    Add Lighthouse shell tests.

--- src/audit/lighthouse/lighthouse.go
+++ src/audit/lighthouse/lighthouse.go
@@ -5,15 +5,19 @@ import (
 	"github.com/xwp/go-tide/src/message"
 	"fmt"
 	"os/exec"
-	"os"
 	"io/ioutil"
-	"log"
 	"encoding/json"
+	"errors"
+)
+
+var (
+	// Using exec.Command as a variable so that we can mock it in tests.
+	execCommand = exec.Command
 )
 
 // Report contains a truncated version of the full report.
 type Report struct {
-	ReportCategories  []ReportCategory `json:"reportCategories"`
+	ReportCategories []ReportCategory `json:"reportCategories"`
 }
 
 // ReportCategory contains the results for each category without the audits.
@@ -33,6 +37,7 @@ func (p Processor) Process(msg message.Message, result *audit.Result) {
 
 	// Cannot perform indexing on the *result directly, so assigning pointer to a local variable.
 	r := *result
+	var results *Report
 
 	// Note: This assumes the shell script `lh` is in $PATH and contains the following command:
 	// `lighthouse --quiet --chrome-flags="--headless --disable-gpu --no-sandbox" --output=json --output-path=stdout $@`
@@ -40,41 +45,46 @@ func (p Processor) Process(msg message.Message, result *audit.Result) {
 	cmdArgs := []string{fmt.Sprintf("https://wp-themes.com/%s", msg.Slug)}
 
 	// Prepare the command and set the stdOut pipe.
-	cmd := exec.Command(cmdName, cmdArgs...)
+	cmd := execCommand(cmdName, cmdArgs...)
+
 	cmdReader, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Println(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-		r["lighthouse"] = err
-		return
-	}
+	errCheck(err, result)
+
+	errReader, err := cmd.StderrPipe()
+	errCheck(err, result)
 
 	// Start the command.
 	err = cmd.Start()
-	if err != nil {
-		log.Println(os.Stderr, "Error starting Cmd", err)
-		r["lighthouse"] = err
-		return
-	}
+	errCheck(err, result)
 
 	// Read stdout pipe.
-	resultBytes, _ := ioutil.ReadAll(cmdReader)
+	resultBytes, err := ioutil.ReadAll(cmdReader)
+	errCheck(err, result)
+
+	// Read stderr pipe.
+	errorBytes, err := ioutil.ReadAll(errReader)
+	errCheck(err, result)
+
+	if len(errorBytes) > 0 {
+		r["lighthouse"] = results
+		r["lighthouseError"] = errors.New("lighthouse command failed: " + string(errorBytes))
+		return
+	}
 
 	// Wait for command to exit and stdio to be read.
 	err = cmd.Wait()
-	if err != nil {
-		log.Println(os.Stderr, "Error waiting for Cmd", err)
-		r["lighthouse"] = err
-		return
-	}
+	errCheck(err, result)
 
 	// Unmarshal the body response into a LightHouseReport object.
-	var results *Report
 	err = json.Unmarshal(resultBytes, &results)
-	if err != nil {
-		log.Println(os.Stderr, "Error validating report", err)
-		r["lighthouse"] = err
-		return
-	}
+	errCheck(err, result)
 
 	r["lighthouse"] = results
+}
+
+func errCheck(err error, result *audit.Result) {
+	r := *result
+	if err != nil {
+		r["lighthouseError"] = err
+	}
 }
--- src/audit/lighthouse/lighthouse_test.go
+++ src/audit/lighthouse/lighthouse_test.go
@@ -5,23 +5,160 @@ import (
 
 	"github.com/xwp/go-tide/src/audit"
 	"github.com/xwp/go-tide/src/message"
+	"os"
+	"os/exec"
+	"fmt"
 )
 
+func mockExecCommand(command string, args ...string) *exec.Cmd {
+	cs := []string{"-test.run=TestHelperProcess", "--", command}
+	cs = append(cs, args...)
+	cmd := exec.Command(os.Args[0], cs...)
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	return cmd
+}
+
 func TestProcessor_Process(t *testing.T) {
+
+	// Set out execCommand variable to the mock function.
+	execCommand = mockExecCommand
+	// Remember to set it back after the test.
+	defer func() { execCommand = exec.Command }()
+
+	result := &audit.Result{}
+
 	type args struct {
 		msg    message.Message
 		result *audit.Result
 	}
 	tests := []struct {
-		name string
-		p    Processor
-		args args
+		name    string
+		p       *Processor
+		args    args
+		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Successful Report",
+			p:    &Processor{},
+			args: args{
+				msg:    message.Message{},
+				result: result,
+			},
+			wantErr: false,
+		},
+		{
+			name: "JSON Error",
+			p:    &Processor{},
+			args: args{
+				msg:    message.Message{Slug: "jsonError"},
+				result: result,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Lighthouse Command Error",
+			p:    &Processor{},
+			args: args{
+				msg:    message.Message{Slug: "error"},
+				result: result,
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.p.Process(tt.args.msg, tt.args.result)
+			r := *tt.args.result
+			if (r["lighthouseError"] != nil && ! tt.wantErr) ||
+				(r["lighthouseError"] == nil && tt.wantErr) {
+					t.Errorf("lighthouse.Process: Error %v, WantErr %v", r["lighthouseError"], tt.wantErr)
+			}
 		})
 	}
+}
+
+// TestHelperProcess is the fake command.
+func TestHelperProcess(t *testing.T) {
+
+	// If the helper process var is not set this code should not run.
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+	// Exit helper sub routine if nothing else exits.
+	defer os.Exit(0)
+
+	// Get the passed arguments.
+	args := os.Args
+	for len(args) > 0 {
+		if args[0] == "--" {
+			args = args[1:]
+			break
+		}
+		args = args[1:]
+	}
+
+	// If no arguments, write to Stderr and exit
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "No command\n")
+		os.Exit(2)
+	}
+
+	cmd, args := args[0], args[1:]
+
+	switch cmd {
+
+	case "lh":
+		switch args[0] {
+		case "https://wp-themes.com/error":
+			fmt.Fprintf(os.Stderr, "Error occurred.")
+			os.Exit(1)
+		case "https://wp-themes.com/jsonError":
+			fmt.Fprintf(os.Stdout, "Invalid json.")
+			os.Exit(0)
+		default:
+			fmt.Fprintf(os.Stdout, exampleReport())
+			os.Exit(0)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command %q\n", cmd)
+		os.Exit(2)
+	}
+}
+
+func exampleReport() string {
+	return `{
+  "reportCategories": [
+    {
+      "name": "Performance",
+      "description": "These encapsulate your web app's current performance and opportunities to improve it.",
+      "id": "performance",
+      "score": 72.17647058823529
+    },
+    {
+      "name": "Progressive Web App",
+      "weight": 1,
+      "description": "These checks validate the aspects of a Progressive Web App, as specified by the baseline [PWA Checklist](https://developers.google.com/web/progressive-web-apps/checklist).",
+      "id": "pwa",
+      "score": 54.54545454545455
+    },
+    {
+      "name": "Accessibility",
+      "description": "These checks highlight opportunities to [improve the accessibility of your web app](https://developers.google.com/web/fundamentals/accessibility). Only a subset of accessibility issues can be automatically detected so manual testing is also encouraged.",
+      "id": "accessibility",
+      "score": 100
+    },
+    {
+      "name": "Best Practices",
+      "description": "We've compiled some recommendations for modernizing your web app and avoiding performance pitfalls.",
+      "id": "best-practices",
+      "score": 81.25
+    },
+    {
+      "name": "SEO",
+      "description": "These checks ensure that your page is optimized for search engine results ranking. There are additional factors Lighthouse does not check that may affect your search ranking. [Learn more](https://support.google.com/webmasters/answer/35769).",
+      "id": "seo",
+      "score": 90
+    }
+  ]
+}`
 }
