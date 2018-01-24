68572eb91a93a31e13d78ee7da58e4b803af74c4
Rheinard Korf <rheinard@thekorfs.com>
2018-01-24 17:18:14 +1100

    Initial 'tide' process tests.

--- src/audit/tide/tide.go
+++ src/audit/tide/tide.go
@@ -4,6 +4,9 @@ import (
 	"github.com/xwp/go-tide/src/audit"
 	"github.com/xwp/go-tide/src/message"
 	"github.com/xwp/go-tide/src/tide"
+	"github.com/pkg/errors"
+	"github.com/xwp/go-tide/src/util"
+	"encoding/json"
 )
 
 type Processor struct{}
@@ -18,16 +21,73 @@ func (p Processor) Process(msg message.Message, result *audit.Result) {
 	// Cannot perform indexing on the *result directly, so assigning pointer to a local variable.
 	r := *result
 
-	// @TODO: Add environment vars for Tide API
+	var client *tide.ClientInterface
+
+	// Attempt to get the client.
 	if r["client"] != nil {
-		client := r["client"].(*tide.ClientInterface)
-		p.sendResults(client,"","")
+		client = r["client"].(*tide.ClientInterface)
 	}
 
-	r["tide"] = "tide"
+	// If we have no client there is nothing we can do.
+	if client == nil {
+		r["tideError"] = errors.New("Tide API client not provided")
+		return
+	}
+
+	if r["audits"] == nil {
+		r["tideError"] = errors.New("there are no audits to send results for")
+		return
+	}
+
+	// Get the right endpoint to send results to.
+	var endpoint string
+	if util.IsCollectionEndpoint(msg.ResponseAPIEndpoint) && r["tideItem"] != nil {
+		// If we found an item on a collection endpoint we will use the checksum endpoint.
+		endpoint = msg.ResponseAPIEndpoint + "/" + r["checksum"].(string)
+	} else {
+		// Use the given endpoint.
+		endpoint = msg.ResponseAPIEndpoint
+	}
+
+	audits := r["audits"].([]string)
+	resultMap := map[string]tide.AuditResult{}
+
+	// Loop through the audits and assign results to the result map.
+	for _, audit := range audits {
+		if r[audit] != nil {
+			if a, ok := r[audit].(*tide.AuditResult); ok {
+				resultMap[audit] = *a
+			}
+		}
+	}
+
+	// Prepare Item for Payload.
+	payloadItem := &tide.Item{
+		Title:       msg.Title,
+		Version:     "",
+		Checksum:    "",
+		Visibility:  msg.Visibility,
+		ProjectType: msg.ProjectType,
+		SourceUrl:   msg.SourceURL,
+		SourceType:  msg.SourceType,
+		CodeInfo:    tide.CodeInfo{},
+		Results:     resultMap,
+	}
+
+	if checksum, ok := r["checksum"].(string); ok {
+		payloadItem.Checksum = checksum
+	}
+
+
+	payload, _ := json.Marshal(payloadItem)
+
+	_, err := p.sendResults(client, endpoint, string(payload))
+	if err != nil {
+		r["tideError"] = err
+	}
 }
 
-func (p Processor) sendResults(client *tide.ClientInterface, endpoint, data string) {
+func (p Processor) sendResults(client *tide.ClientInterface, endpoint, data string) (string, error) {
 	c := *client
-	c.SendPayload("POST", endpoint, data)
+	return c.SendPayload("POST", endpoint, data)
 }
--- src/audit/tide/tide_test.go
+++ src/audit/tide/tide_test.go
@@ -5,9 +5,50 @@ import (
 
 	"github.com/xwp/go-tide/src/audit"
 	"github.com/xwp/go-tide/src/message"
+	"github.com/xwp/go-tide/src/tide"
+	"fmt"
 )
 
+var (
+	TideClient tide.ClientInterface
+)
+
+type MockClient struct{}
+
+func (m MockClient) Authenticate(clientId, clientSecret, authEndpoint string) error {
+	return nil
+}
+
+func (m MockClient) SendPayload(method, endpoint, data string) (string, error) {
+
+	// @todo Remove after debug.
+	fmt.Println(data)
+
+	return "", nil
+}
+
 func TestProcessor_Process(t *testing.T) {
+
+	TideClient = &MockClient{}
+
+	fullCollectionMessage := message.Message{
+		ResponseAPIEndpoint: "http://example/api/tide/v1/audit",
+		Title:               "Dummy Theme",
+		Content:             "Dummy theme for testing",
+		Slug:                "dummy-theme",
+		ProjectType:         "theme",
+		SourceURL:           "http://themerepo/dummy-theme-1.3.zip",
+		SourceType:          "zip",
+		RequestClient:       "wporg",
+		Force:               false,
+		Visibility:          "public",
+	}
+
+	fullItemMessage := fullCollectionMessage
+	fullItemMessage.ResponseAPIEndpoint += "/39c7d71a68565ddd7b6a0fd68d94924d0db449a99541439b3ab8a477c5f1fc4e"
+
+	fullResult := getFullResult()
+
 	type args struct {
 		msg    message.Message
 		result *audit.Result
@@ -17,7 +58,14 @@ func TestProcessor_Process(t *testing.T) {
 		p    Processor
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			"Collection Audit - Full",
+			Processor{},
+			args{
+				fullCollectionMessage,
+				fullResult,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -33,4 +81,70 @@ func TestProcessor_Kind(t *testing.T) {
 			t.Errorf("Processor.Kind() = %v, Impossible, this should be tide.", got)
 		}
 	})
+}
+
+func getFullResult() *audit.Result {
+	return &audit.Result{
+		"client": &TideClient,
+		"audits": []string{
+			"lighthouse",
+		},
+		"tempFolder": "/tmp",
+
+		// Added by "ingest" process.
+		"ingest":      &Processor{},
+		"ingestError": nil,
+		"tideItem": &tide.Item{
+			Title:       "Dummy Plugin",
+			Version:     "1.3",
+			Checksum:    "39c7d71a68565ddd7b6a0fd68d94924d0db449a99541439b3ab8a477c5f1fc4e",
+			Visibility:  "public",
+			ProjectType: "theme",
+			SourceUrl:   "http://themerepo/dummy-theme-1.3.zip",
+			SourceType:  "zip",
+		},
+		"checksum": "39c7d71a68565ddd7b6a0fd68d94924d0db449a99541439b3ab8a477c5f1fc4e",
+		"files":    []string{"file1.txt", "file2.txt", "file3.txt"},
+
+		// Added by "lighthouse" process.
+		"lighthouse": &tide.AuditResult{
+			Summary: &tide.AuditSummary{
+				LighthouseSummary: &tide.LighthouseSummary{
+					ReportCategories: []tide.LighthouseCategory{
+						{
+							Name:        "Performance",
+							Description: "These encapsulate your web app's current performance and opportunities to improve it.",
+							Id:          "performance",
+							Score:       72.17647058823529,
+						},
+						{
+							Name:        "Progressive Web App",
+							Description: "These checks validate the aspects of a Progressive Web App, as specified by the baseline [PWA Checklist](https://developers.google.com/web/progressive-web-apps/checklist).",
+							Id:          "pwa",
+							Score:       72.17647058823529,
+						},
+						{
+							Name:        "Accessibility",
+							Description: "These checks highlight opportunities to [improve the accessibility of your web app](https://developers.google.com/web/fundamentals/accessibility). Only a subset of accessibility issues can be automatically detected so manual testing is also encouraged.",
+							Id:          "accessibility",
+							Score:       100.00,
+						},
+						{
+							Name:        "Best Practices",
+							Description: "We've compiled some recommendations for modernizing your web app and avoiding performance pitfalls.",
+							Id:          "best-practices",
+							Score:       81.25,
+						},
+						{
+							Name:        "SEO",
+							Description: "These checks ensure that your page is optimized for search engine results ranking. There are additional factors Lighthouse does not check that may affect your search ranking. [Learn more](https://support.google.com/webmasters/answer/35769).",
+							Id:          "seo",
+							Score:       90,
+						},
+					},
+				},
+			},
+		},
+		"lighthouseError": nil,
+	}
 }
