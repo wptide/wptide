3f80015a60d4193401d7c7f34f7452785f9108f2
Derek Herman <derek@valendesigns.com>
2018-06-03 15:24:03 -0700

    Do not load caching files if not turned on

--- service/api/wp-content/advanced-cache.php
+++ service/api/wp-content/advanced-cache.php
@@ -6,4 +6,6 @@
  */
 
 // Windows-friendly symlink.
-require_once WP_CONTENT_DIR . '/plugins/pj-page-cache-red/advanced-cache.php';
+if ( defined( 'WP_CACHE' ) && true === WP_CACHE ) {
+	require_once WP_CONTENT_DIR . '/plugins/pj-page-cache-red/advanced-cache.php';
+}
