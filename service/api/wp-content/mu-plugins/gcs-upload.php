afa53341f1faf755e081aa3dab806ab0d02f821f
Utsav Tilava <utsavt@hyperlinkinfosystem.net.in>
2023-04-19 18:19:51 +0530

    Never use shorthand PHP start tags. Always use full PHP tags.

--- service/api/wp-content/mu-plugins/gcs-upload.php
+++ service/api/wp-content/mu-plugins/gcs-upload.php
@@ -73,7 +73,7 @@ class Upload {
 		}
 		?>
 		<div class="wrap">
-			<h1><?= esc_html( get_admin_page_title() ); ?></h1>
+			<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 			<form action="options.php" method="POST">
 				<?php
 
